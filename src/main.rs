90ee61f6bdaa55296ee3b9ba47a8ea21fd1f44ad
pmarshall111 <pmarshall.dev@gmail.com>
2024-07-21 15:13:55 +0100

    After testing chapter

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,59 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-
+use rust_os::println;
 use core::panic::PanicInfo;
 
-// static HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
     // this function is the entry point
     // _start is called before main and usually does things like:
     // - setting up the stack
     // - 
 
-    print!("{}", 'H');
-    print!("ello ");
-    print!("Wörld!");
     println!("hi\n\nWhatz up dawg");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
